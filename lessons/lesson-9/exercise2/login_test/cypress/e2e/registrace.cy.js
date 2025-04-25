adaf0ac9f96bfc4c2e44cfa05b0ec1f60f98fc9a
LiudmylaSeveryna <lyusitta@meta.ua>
2025-04-25 08:12:38 +0200

    change_less10

--- lessons/lesson-9/exercise2/login_test/cypress/e2e/registrace.cy.js
+++ lessons/lesson-9/exercise2/login_test/cypress/e2e/registrace.cy.js
@@ -1,17 +1,18 @@
 //npm install --save-dev @faker-js/faker
-
-import { faker } from '@faker-js/faker';
-
-describe('template spec', () => {
+  describe('template spec', () => {
     it('passes', () => {
-      cy.visit('http://127.0.0.1:5500/lessons/lesson-9/exercise2/registration.html')
+      cy.fixture('environ.json').then((environs) => {
+        cy.visit(environ.url2)
   // act
-  cy.fixture('user.json').then((user) => {
+      cy.fixture('user.json').then((user) => {
       cy.get('input[data-testid="reg-username-input"]').type(user.username)
       cy.get('input[data-testid="login-password-input"]').type(user.password)
       cy.get('[data-testid="login-submit-button"]').click()
   
       cy.get('[data-testid="login-success-message"]').should('be.visible')
       })
+    })
+
+
     })
   })
